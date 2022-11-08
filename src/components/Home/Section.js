044038c1bf0283adb6c7e7e66011e440962ac538
anisa6121 <you@example.com>
2022-11-08 20:34:43 +0600

    footer-add

--- src/components/Home/Section.js
+++ src/components/Home/Section.js
@@ -5,7 +5,7 @@ import { RiSecurePaymentLine, Ri24HoursLine } from "react-icons/ri";
 import img1 from '../../assets/img2.jpg'
 const Section = () => {
     return (
-		<div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 mt-4 bg-gray-100">
+		<div className="mb-6 relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 mt-4 bg-gray-100">
 			<div className="relative">
 				<div className="grid gap-12 row-gap-8 lg:grid-cols-2">
 					<div>
