c3986a23dd6e7bb522f0984f1073618ca0b6c117
anisa6121 <you@example.com>
2022-11-08 19:03:18 +0600

    add-hero-section

---
+++ src/components/Home/Hero.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import img from '../../assets/img.jpg'
+const Hero = () => {
+    return (
+		<div className="bg-white-800 text-black-100">
+			<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+				<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+					<h1 className="text-4xl font-bold leading-none sm:text-6xl">
+						The Fastest Delivery
+						<span className="m-2  text-orange-400">
+							in Your City
+						</span>
+					</h1>
+
+					<p className="mt-6 mb-8 text-2xl sm:mb-12">
+						Get our services for better quality
+						food and fast delivery.
+						<br className="hidden md:inline lg:hidden" />
+						Our mission is to bring healthy food
+						to everyone.
+					</p>
+
+					<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+						<a
+							rel="noopener noreferrer"
+							href="/"
+							className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 text-gray-900"
+						>
+							See Our Foods
+						</a>
+					</div>
+				</div>
+
+				<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+					<img
+						src={img}
+						alt=""
+						className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+					/>
+				</div>
+			</div>
+		</div>
+    );
+};
+
+export default Hero;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Slider from '../Slider/Slider';
+import Hero from './Hero';
 
 const Home = () => {
     return (
         <div className=''>
+            <Hero></Hero>
            <Slider></Slider>
         </div>
     );
